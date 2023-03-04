7d9b6159a9d3e9742caeba2867ee560d7c19518a
sam_dev <samueldevpro09@gmail.com>
2023-03-04 01:37:56 +0100

    google analytics cahnges moving tag to the top

--- pages/_document.js
+++ pages/_document.js
@@ -4,22 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        ></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        ></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
-          rel="stylesheet"
-        ></link>
-
         {/* google analytics */}
         <script
           async
@@ -37,6 +21,22 @@ export default function Document() {
             `,
           }}
         />
+
+        {/* fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
 
       <body>
